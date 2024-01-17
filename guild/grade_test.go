f87c5af5d8e8c8470caee0bb441cd66e18d32df3
伍六七 <754162382@qq.com>
2024-01-17 18:28:33 +0800

    Modify implementation methods

--- guild/grade_test.go
+++ guild/grade_test.go
@@ -12,15 +12,8 @@ func TestGrade(t *testing.T) {
 	c := config.New("config_temp")
 	d := db.New(c)
 	g := New(c, d)
-	c = config.New("config_temp2")
-	d = db.New(c)
-	translator, err := d.GetTranslator("d8c270f68a8f44aaa6b24e17c927df2b", nil)
-	if err != nil {
-		fmt.Print(err)
-	}
-	fmt.Println(translator)
 	start := time.Now().AddDate(0, 0, -28).Format("2006-01-02")
 	end := time.Now().Format("2006-01-02")
-	err = g.GenGrade("e8d79c55c0394cba83664f3e5737b0bd", "d8c270f68a8f44aaa6b24e17c927df2b", start, end)
+	err := g.GenGrade("e8d79c55c0394cba83664f3e5737b0bd", "d8c270f68a8f44aaa6b24e17c927df2b", start, end)
 	fmt.Println(err)
 }
